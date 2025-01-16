c40e98db37e8eac1b5d9236c064339e0aa2fe332
loicr <loic.recoupe@gmail.com>
2025-01-16 15:11:28 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MainScene from './components/Scene/MainScene'
+
+function App() {
+  return (
+    <div className="fixed inset-0 w-full h-full bg-black">
+      <MainScene />
+    </div>
+  )
+}
+
+export default App
